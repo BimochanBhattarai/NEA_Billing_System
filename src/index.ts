a01b2c0e44d8a162ded9101fddc46038e0811b3a
Bimochan Bhattarai <itsbimochan@gmail.com>
2025-06-24 12:03:35 +0545

    Working State of Prisma

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from './generated/prisma'
+
+const prisma = new PrismaClient()
+
+
+
+async function main() {
+
+  await prisma.userType.create({
+    data: {
+      Name: 'Administrator',
+      Status: true
+    },
+  })
+
+  await prisma.user.create({
+    data: {
+        Email: 'alice@prisma.io',
+        Password: 'password123',
+        Status: true,
+        UserId: 'alice123',
+        UserName: 'alice',
+        Name: 'Alice',
+        UserType: {
+          connect: {
+            Name: 'Administrator',
+          },
+        },
+    }})
+
+  const allUsers = await prisma.user.findMany()
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
